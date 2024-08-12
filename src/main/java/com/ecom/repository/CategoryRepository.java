3b85a5f0df9ddf6ea2b5b90439f9d33c78707f3b
hshalash <hshalash@arpuplus.com>
2024-08-12 17:54:05 +0300

    first commit for shopping website

---
+++ src/main/java/com/ecom/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.ecom.repository;
+
+import com.ecom.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	public Boolean existsByName(String name);
+
+	public List<Category> findByIsActiveTrue();
+
+}
